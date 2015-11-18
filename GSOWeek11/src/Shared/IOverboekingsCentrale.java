77e2042f0e7112408976f247bc4112f6501a14d0
salami350 <salami350@live.nl>
2015-11-18 09:12:05 +0100

    javadoc Bank.java and OverboekingsCentrale

--- GSOWeek11/src/Shared/IOverboekingsCentrale.java
+++ GSOWeek11/src/Shared/IOverboekingsCentrale.java
@@ -11,5 +11,13 @@ package Shared;
  */
 public interface IOverboekingsCentrale
 {
-    
+
+    /**
+     * @param IBAN het IBAN van de rekening van de klant die het bedrag overmaakt.
+     * @param tegenIBAN het IBAN van de rekening van de klant die het bedrag ontvangt
+     * @param amount het geldbedrag dat overgemaakt wordt.
+     * @return int, voltooingscode, 0 is succcesfull, 1 is error #1, 2 is another error #2, etc
+     * substracts the amount from the rekening with IBAN en adds it to the rekening with tegenIBAN and returns int 0, unless the saldo of the rekening with IBAN is smaller than amount then it returns an error code.
+     */
+    public int maakOver(String IBAN, String tegenIBAN, double amount);
 }
